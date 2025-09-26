73c487c2bfd270dcc7a6774418effb4222c8d086
Jumborin <y.f.architect@gmail.com>
2025-09-27 00:18:05 +0900

    不具合修正

--- cypress/test/e2e/redmine/AllScreenCatures.cy.js
+++ cypress/test/e2e/redmine/AllScreenCatures.cy.js
@@ -2,21 +2,23 @@ describe('Redmineの全画面の画面表示テスト', () => {
 
   beforeEach(() => {
     cy.visit('http://158.179.191.24/');
+    cy.get('#account ul li a.login').click();
+    cy.get('input#username').type('admin'); //ユーザ名入力ボックス
+    cy.get('input#password').type('p@ssw0rd'); //パスワード入力ボックス
+    cy.get('input[name="login"]').click(); //ログインボタン
+
   });
   afterEach(function () {
     cy.screenshot(this.currentTest.title);
   });
 
-  describe('上部メニュー遷移テスト', () => {
+  describe('ログインテスト', () => {
     it('ログインリンクをクリックし、ログイン画面に遷移すること', function () {
-      cy.get('#account ul li a.login').click();
-      cy.get('input#username').type('admin'); //ユーザ名入力ボックス
-      cy.get('input#password').type('p@ssw0rd'); //パスワード入力ボックス
-      cy.get('input[name="login"]').click(); //ログインボタン
       cy.get('#loggedas').should('contain', 'admin'); //ログイン確認
-    
     });
+  });
 
+  describe('上部メニュー遷移テスト', () => {
     it('ホームリンクをクリックし、ホーム画面に遷移すること', function () {
       cy.get('#top-menu ul li a.home').click();
     });
