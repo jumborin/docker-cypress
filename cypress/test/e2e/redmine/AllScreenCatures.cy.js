c69f3acb397c6300ec869440c8b9fc70e31f982f
Jumborin <y.f.architect@gmail.com>
2025-09-27 00:28:59 +0900

    ページ遷移後の確認処理を追加

--- cypress/test/e2e/redmine/AllScreenCatures.cy.js
+++ cypress/test/e2e/redmine/AllScreenCatures.cy.js
@@ -21,18 +21,23 @@ describe('Redmineの全画面の画面表示テスト', () => {
   describe('上部メニュー遷移テスト', () => {
     it('ホームリンクをクリックし、ホーム画面に遷移すること', function () {
       cy.get('#top-menu ul li a.home').click();
+      cy.url().should('include', '158.179.191.24');
     });
     it('マイページリンクをクリックし、マイページ画面に遷移すること', function () {
       cy.get('#top-menu ul li a.my-page').click();
+      cy.url().should('include', '/my/page');
     });
     it('プロジェクトリンクをクリックし、プロジェクト一覧画面に遷移すること', function () {
       cy.get('#top-menu ul li a.projects').click();
+      cy.url().should('include', '/projects');
     });
     it('管理リンクをクリックし、管理画面に遷移すること', function () {
       cy.get('#top-menu ul li a.administration').click();
+      cy.url().should('include', '/admin');
     });
-    it('ヘルプリンクをクリックし、ヘルプ画面に遷移すること', function () {
+    it('ヘルプリンクをクリックし、Redmineガイドに遷移すること', function () {
       cy.get('#top-menu ul li a.help').click();
+      cy.url().should('include', 'https://www.redmine.org/guide');
     });
   });
 });
